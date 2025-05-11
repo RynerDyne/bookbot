ad555e0b2745474d009a2eab4520d80a0fe9f5ce
reynardine <dominic@mightyexplorers.com>
2025-05-11 09:15:42 -0500

    finish bookbot project

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from stats import count_string, character_instance_data, sort_dictionary_of_characters
+from stats import count_string, character_instance_data_alpha, sort_dictionary_of_characters
+import sys
+
+
+# Graceful program exit if too many, or not enought arguments
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,18 +16,18 @@ def get_book_text(file_path):
     return contents
 
 def main():
-    frankenstein_txt = get_book_text('./books/frankenstein.txt')
-    frankenstein_word_count = count_string(frankenstein_txt)
-    frankenstein_character_list_alpha = character_instance_data(frankenstein_txt)
-    frankenstein_sorted_count = sort_dictionary_of_characters(frankenstein_character_list_alpha)
+    book_txt = get_book_text(book_path)
+    book_word_count = count_string(book_txt)
+    book_character_list_alpha = character_instance_data_alpha(book_txt)
+    book_sorted_count = sort_dictionary_of_characters(book_character_list_alpha)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {frankenstein_word_count} total words")
+    print(f"Found {book_word_count} total words")
     print("--------- Character Count -------")
 
-    for dict in frankenstein_sorted_count:
+    for dict in book_sorted_count:
         if dict['char'].isalpha():
             print(f"{dict['char']}: {dict['num']}")
 
